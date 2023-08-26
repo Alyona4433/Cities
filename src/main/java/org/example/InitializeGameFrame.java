b39db4eb52e24fb812947fc16fa8b76bdfac49d6
Alyona4433 <alena4433@gmail.com>
2023-08-26 19:18:19 +0300

    first commit

--- src/main/java/org/example/InitializeGameFrame.java
+++ src/main/java/org/example/InitializeGameFrame.java
@@ -42,7 +42,7 @@ public class InitializeGameFrame {
         makeMoveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Add your logic here for processing user input and generating computer response
+
             }
         });
 
